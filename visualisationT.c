cff6146f117bb3652d19650efd80d590b34e520c
AlexandreR4 <alexandre.radin@isen-ouest.yncrea.fr>
2023-03-06 15:29:35 +0100

    écriture_temperature

--- visualisationT.c
+++ visualisationT.c
@@ -1,15 +1,39 @@
 #include "visualisationT.h"
 
 #include <unistd.h>
+#include <stdio.h>
 
 // mettre les valeurs de myTemp dans le file
 void visualisationT(temp_t myTemp)
 {
-    if( access( ".verrouData", F_OK ) != -1 ){
-        // Fichier existe
+    if(access( ".verrouData", F_OK ) != -1){
+        // Fichier verrou existe
     }
     else{
-        // Fichier n'existe pas
+        // Fichier verrou n'existe pas
+        // crée le fichier verrou
+        FILE* fichier_verrou = fopen(".verrouData", "w+");
+        fclose(fichier_verrou);
+        // écrit les températures intérieures et extérieures dans le fichier data.txt
+        FILE* flux;
+        flux = fopen("data.txt", "r+"); // r+ : les données précédentes ne sont pas effacées si le fichier existe
+        if(flux == NULL){
+            printf("Error in opening file");
+            return 1;
+        }
+        else{
+            // extrait du fichier le témoin de chauffe
+            char* temoin_de_chauffe;
+            fscanf(flux, "\n\n%s", temoin_de_chauffe); 
+            fclose(flux);
+            flux = fopen("data.txt", "w+"); // w+ : les données précédentes sont effacées si le fichier existe
+            // écrit dans le fichier
+            float exterieure = myTemp.exterieure;
+            float interieure = myTemp.interieure;
+            fprintf(flux, "%f\n%f\n%s", exterieure, interieure, temoin_de_chauffe);
+            fclose(flux);
+            // supprime fichier verrou
+            remove(".verrouData");
+        }
     }
-    remove(".verrouData")
 }
