3746f1fa3cfdf4902c4c800ce90ce16d58b3c0a5
MaxenceRogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2023-03-06 15:15:40 +0100

    imp consigne.c

--- consigne.c
+++ consigne.c
@@ -1,12 +1,24 @@
+#include <unistd.h>
 #include "consigne.h"
+#include <unistd.h>
+
 
    float consigne(float thermostatPrec_f)
    {
-        float consigne;
-        FILE* data;
-        data = fopen("consigne.txt", "r");
-        fscanf(data, "%f", consigne);
-        return consigne;
+       if( access( ".verrouConsigne", F_OK ) != 1 ){
+// Fichier existe
+            return thermostatPrec_f; //si pas de fichier verrou, la valeur précedente du thermostat est return
+       }else{
+// Fichier n'existe pas
+           fopen(".verrouConsigne","w+"); //mode w+ pour créer le fichier s'il n'existe pas
+           float consigne;
+           FILE* data;
+           data = fopen("consigne.txt", "r");
+           fscanf(data, "%f", consigne);
+           remove(".verrouConsigne");
+           return consigne;
+   }
+
 
 
       //float thermostat_f=0;
