92ee4fbb0c67a45f971be4b48919914361adf1e8
AlexandreR4 <alexandre.radin@isen-ouest.yncrea.fr>
2023-03-06 11:17:13 +0100

    visualisation_temperature

--- visualisationT.c
+++ visualisationT.c
@@ -1,6 +1,14 @@
 #include "visualisationT.h"
 
+#include <unistd.h>
+
 void visualisationT(temp_t myTemp)
 {
-   
+    if( access( ".verrouData", F_OK ) != -1 ){
+        // Fichier existe
+    }
+    else{
+        // Fichier n'existe pas
+    }
+    remove(".verrouData")
 }
