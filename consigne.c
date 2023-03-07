5fe4cf4bb4a27be44e1f5d8b5ade8c053e537fae
MaxenceRogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2023-03-07 12:00:55 +0100

    consigne.c FINAL MARCHE DE FOU

--- consigne.c
+++ consigne.c
@@ -4,7 +4,8 @@
 
 float consigne(float thermostatPrec_f)
 {
-   if( access( ".verrouConsigne", F_OK ) != 1 ){
+   float consigne;
+   if( access( ".verrouConsigne", F_OK ) != -1 ){
       // Fichier existe
       return thermostatPrec_f; //si pas de fichier verrou, la valeur précedente du thermostat est return
    }
@@ -14,21 +15,20 @@ float consigne(float thermostatPrec_f)
       FILE* fichier_verrou = fopen(".verrouConsigne", "w+");
       fclose(fichier_verrou);
       // récupération de la consigne
-      float consigne;
-      FILE* data=NULL;
-      data = fopen("consigne.txt", "r");
-      if(data == NULL){
-            printf("Error in opening file");
-            return 1;
+
+      FILE* dataFile=NULL;
+       dataFile = fopen("consigne.txt", "r+");
+      if(dataFile == NULL){
+            printf("Error in opening file\n");
+
       }
       else{
-          printf("Info : %f\n",consigne);
-         fscanf(data, "%f", consigne);
-          printf("Info : %f\n",consigne);
-         fclose(data);
-         // suppression fichier verrou
+          char ligne[8];
+          fgets(ligne, 8, dataFile);
+          consigne = strtof(ligne,NULL);
+          fclose(dataFile);
       }
-      remove(".verrouConsigne");
+      remove(".verrouConsigne");// suppression fichier verrou
       return consigne;
    }
 //float thermostat_f=0;
