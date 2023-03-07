eb144b85031d5ede3bcb197851e8d591931eacb1
MaxenceRogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2023-03-07 08:58:44 +0100

    changes in autotests

--- cmake-build-debug/CMakeFiles/A3projetC.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/A3projetC.dir/cmake_clean.cmake
@@ -1,11 +1,11 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/A3projetC.dir/autotests.c.o"
   "CMakeFiles/A3projetC.dir/consigne.c.o"
   "CMakeFiles/A3projetC.dir/regulation.c.o"
   "CMakeFiles/A3projetC.dir/simulateur.c.o"
   "CMakeFiles/A3projetC.dir/test_simulateur.c.o"
   "CMakeFiles/A3projetC.dir/visualisationC.c.o"
   "CMakeFiles/A3projetC.dir/visualisationT.c.o"
+  "CMakeFiles/A3projetC.dir/autotests.c.o"
   "A3projetC.pdb"
   "A3projetC"
 )
