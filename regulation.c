4b4e6defdb74ea50b6075bb07f2ac4a6631ae20a
MaxenceRogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2023-03-07 16:17:30 +0100

    modif PID regulation.c

--- regulation.c
+++ regulation.c
@@ -9,16 +9,28 @@
             return 0;
         }
 }
-    float PID(float consigne, float tempA, float tempB, float temperature){
-        float P = consigne-temperature;
-        float I = 0;
-        float D = (tempB-tempA)/4;
+    float PID(float error,float errorSum,float errorVar){
+        float Kp = 1;
+        float Ki = 0.1;
+        float Kd = 0.1;
 
-        if(temperature<consigne){
+        float sum = 0;
 
-        } else {
+        float P = error*Kp;
+        float I = errorSum*Ki;
+        float D = errorVar*Kd;
 
-        }
+//        if(posnT >= 0){
+//            D = (( (consigne-(tabT[nT])) - (consigne-tabT[nT-1])) /4)*Kd;
+//        } else {
+//            D = (( (consigne-(tabT[0])) - (consigne-tabT[1])) /4)*Kd;
+//        }
+
+        float pid = P+I+D;
+
+        //printf("PID :%f/%f/%f\n",P,I,D);
+
+        return pid;
     }
 	 
 	/* regul : type de régulation (1:Tout ou Rien, 2:PID)
@@ -27,6 +39,11 @@
 		nT : nombre d’entrées dans le tableau tabT*/
 	float regulationTest(int regul,float consigne,float* tabT, int nT){
 		float cmd;
+		float error = 0;
+        float errorSum = 0;
+        float errorVar = 0;
+        float errorPrev = 0;
+
         if(regul == 1){
             for (int i = 0; i < nT; i++) {
                 cmd = TOR(consigne,tabT[i]);
@@ -34,8 +51,15 @@
         }
 
         if(regul == 2){
+            //printf("%f\n",tabT[0]);
             for (int i = 0; i < nT; ++i) {
-                cmd = PID(consigne,tabT[i-2],tabT[i-1],tabT[i]);
+                error = consigne - tabT[i];
+                errorSum += error;
+                errorVar = error-errorPrev;
+                printf("%f\n",tabT[i]);
+                //printf("Errors : %f/%f/%f/%f\n",error,errorSum,errorVar,errorPrev);
+                cmd = PID(error,errorSum,errorVar);
+                errorPrev = error;
             }
         }
 		return cmd;
