a995ecc246a086fc471e1e62e7dddac446fb54a0
AlexandreR4 <alexandre.radin@isen-ouest.yncrea.fr>
2023-03-07 11:30:18 +0100

    regul

--- regulation.c
+++ regulation.c
@@ -1,17 +1,16 @@
-    #include <stdio.h>
-	 #include <stdlib.h>
-    #include "regulation.h"
-	 
-	/* regul : type de régulation (1:Tout ou Rien, 2:PID)
-		csgn : température de consigne
-		tabT : tableau de températures intérieures successives en entrée de la régulation
-		nT : nombre d’entrées dans le tableau tabT*/
-	float regulationTest(int regul,float consigne,float* tabT, int nT){
-		float cmd;
-		
-		return cmd;
-	}
-	/*
-	retourne cmd après nT appels à la fonction regulation()
-	*/
-  
+#include <stdio.h>
+#include <stdlib.h>
+#include "regulation.h"
+
+/* regul : type de régulation (1:Tout ou Rien, 2:PID)
+csgn : température de consigne
+tabT : tableau de températures intérieures successives en entrée de la régulation
+nT : nombre d’entrées dans le tableau tabT*/
+float regulationTest(int regul,float consigne,float* tabT, int nT){
+float cmd;
+
+return cmd;
+}
+/*
+retourne cmd après nT appels à la fonction regulation()
+*/
