27f8ae2697c50dd1bc19c1216fb0ebdc192cc563
AlexandreR4 <alexandre.radin@isen-ouest.yncrea.fr>
2023-03-08 12:00:32 +0100

    backup_ancienne_version_pid

--- regulation.c
+++ regulation.c
@@ -64,4 +64,55 @@
 	/*
 	retourne cmd après nT appels à la fonction regulation()
 	*/
+
+/*
+BACKUP (ANCIENNE VERSION)
+float PID(float error,float errorSum,float errorVar){
+        // def coeff
+        float Kp = 1;
+        float Ki = 0.1;
+        float Kd = 0.1;
+        // def P,I,D
+        float P = error*Kp;
+        float I = errorSum*Ki;
+        float D = errorVar*Kd;
+
+        float pid = P+I+D;
+
+        return pid;
+    }
+	 
+	
+	float regulationTest(int regul,float consigne,float* tabT, int nT){
+		float cmd;
+		float error = 0;
+        float errorSum = 0;
+        float errorVar = 0;
+        float errorPrev = 0;
+        float tempPrev = 0;
+
+        if(regul == 1){
+            for (int i = 0; i < nT; i++) {
+                cmd = TOR(consigne,tabT[i]);
+            }
+        }
+
+        if(regul == 2){
+            for (int i = 1; i < nT; ++i) {
+                error = consigne - tabT[i];
+                if(tabT[i-1]-tabT[i]<0){
+                    errorSum += error*10-((tabT[i-1]-tabT[i])*10/2); //temp decroissante
+                } else {
+                    errorSum += error*10+((tabT[i-1]-tabT[i])*10/2); //temp croissante
+                }
+                errorVar = (error-errorPrev)/10;
+                cmd = PID(error,errorSum,errorVar);
+                errorPrev = error;
+            }
+        }
+		return cmd;
+	}
+
+
+*/
   
