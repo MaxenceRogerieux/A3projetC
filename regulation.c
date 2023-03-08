2a029b11da1ef5a97e6087e298dd5a5d403d6c66
MaxenceRogerieux <maxence.rogerieux@isen-ouest.yncrea.fr>
2023-03-08 09:36:26 +0100

    final PID regulation.c

--- regulation.c
+++ regulation.c
@@ -9,27 +9,20 @@
             return 0;
         }
 }
-    float PID(float error,float errorSum,float errorVar){
+    float PID(float error,float errorSum,float errorVar,float tempPrev){
+        // def coeff
         float Kp = 1;
         float Ki = 0.1;
         float Kd = 0.1;
-
-        float sum = 0;
-
+        // def P,I,D
         float P = error*Kp;
         float I = errorSum*Ki;
         float D = errorVar*Kd;
 
-//        if(posnT >= 0){
-//            D = (( (consigne-(tabT[nT])) - (consigne-tabT[nT-1])) /4)*Kd;
-//        } else {
-//            D = (( (consigne-(tabT[0])) - (consigne-tabT[1])) /4)*Kd;
-//        }
-
         float pid = P+I+D;
 
         //printf("PID :%f/%f/%f\n",P,I,D);
-
+        printf("PID :%f\n",pid);
         return pid;
     }
 	 
@@ -43,6 +36,7 @@
         float errorSum = 0;
         float errorVar = 0;
         float errorPrev = 0;
+        float tempPrev = 0;
 
         if(regul == 1){
             for (int i = 0; i < nT; i++) {
@@ -52,14 +46,16 @@
 
         if(regul == 2){
             //printf("%f\n",tabT[0]);
-            for (int i = 0; i < nT; ++i) {
+            for (int i = 1; i < nT; ++i) {
                 error = consigne - tabT[i];
-                errorSum += error;
-                errorVar = error-errorPrev;
-                printf("%f\n",tabT[i]);
+                errorSum += error*10-((tabT[i-1]-tabT[i])*10/2);
+                //printf("errorSum : %f\n",((tabT[i-1]-tabT[i])*10/2));
+                errorVar = (error-errorPrev)/10;
+                //printf("%f\n",tabT[i]);
                 //printf("Errors : %f/%f/%f/%f\n",error,errorSum,errorVar,errorPrev);
-                cmd = PID(error,errorSum,errorVar);
+                cmd = PID(error,errorSum,errorVar,tempPrev);
                 errorPrev = error;
+                tempPrev = tabT[i];
             }
         }
 		return cmd;
